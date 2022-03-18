674562084fbb086616cdf9f59e3d298714c59d37
Alex Won <dnjscksdn98@gmail.com>
2022-03-18 23:20:57 +0900

    setup database configurations

--- app/database.py
+++ app/database.py
@@ -10,7 +10,14 @@ from sqlalchemy.orm import sessionmaker
 env_path = Path("config") / ".env"
 load_dotenv(dotenv_path=env_path)
 
-SQLALCHEMY_DATABASE_URL = os.getenv("")
+
+DATABASE_USERNAME = os.getenv("MYSQL_USERNAME")
+DATABASE_PASSWORD = os.getenv("MYSQL_PASSWORD")
+DATABASE_HOST = os.getenv("MYSQL_HOST")
+DATABASE_PORT = os.getenv("MYSQL_PORT")
+DATABASE_NAME = os.getenv("MYSQL_DB")
+
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 DBSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
