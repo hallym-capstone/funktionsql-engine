a27b6b5430707cbc322368ce350532852ee92225
Alex Won <dnjscksdn98@gmail.com>
2022-03-26 17:07:24 +0900

    feat: initialize runtime engine codebase

--- app/runtime/engine.py
+++ app/runtime/engine.py
@@ -3,3 +3,15 @@ class RuntimeEngine:
     @classmethod
     def initialize(cls):
         print("[*] initialized Runtime Engine")
+
+    @classmethod
+    def consume_prepared_query(cls):
+        pass
+
+    @classmethod
+    def abstract_relational_query(cls):
+        pass
+
+    @classmethod
+    def publish_to_execution_engine(cls):
+        pass
