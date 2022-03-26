77f8a4dd6a6244e6cb4cb76f7ac89a61ab2ee815
Alex Won <dnjscksdn98@gmail.com>
2022-03-26 17:08:22 +0900

    feat: initialize execution engine codebase

--- app/execution/engine.py
+++ app/execution/engine.py
@@ -3,3 +3,15 @@ class ExecutionEngine:
     @classmethod
     def initialize(cls):
         print("[*] initialized Execution Engine")
+
+    @classmethod
+    def consume_executable_query(cls):
+        pass
+
+    @classmethod
+    def construct_lambda_execution(cls):
+        pass
+
+    @classmethod
+    def run_executable_query(cls):
+        pass
