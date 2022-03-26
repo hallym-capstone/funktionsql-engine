79cadc5260e0c02305d6682f4659dd8dedcfb5cc
Alex Won <dnjscksdn98@gmail.com>
2022-03-26 17:36:52 +0900

    feat: initialize runtime scheduler codebases

--- app/runtime/scheduler.py
+++ app/runtime/scheduler.py
@@ -3,3 +3,15 @@ class RuntimeScheduler:
     @classmethod
     def initialize(cls):
         print("[*] initialized Runtime Scheduler")
+
+    @classmethod
+    def consume_scheduled_query(cls):
+        pass
+
+    @classmethod
+    def abstract_relational_query(cls):
+        pass
+
+    @classmethod
+    def publish_to_execution_engine(cls):
+        pass
