6996412a26e2c9c782bb5ba7eae430350f608cbd
Alex Won <dnjscksdn98@gmail.com>
2022-03-26 17:34:58 +0900

    chore: add query_module functions

--- app/routers/modules/query_module.py
+++ app/routers/modules/query_module.py
@@ -26,3 +26,19 @@ class QueryModule:
     @classmethod
     def publish_to_runtime(cls):
         pass
+
+    @classmethod
+    def get_databases(cls, api_key: str):
+        pass
+
+    @classmethod
+    def get_database(cls, database_name: str, api_key: str):
+        pass
+
+    @classmethod
+    def get_functions(cls, database_name: str, api_key: str):
+        pass
+
+    @classmethod
+    def get_function(cls, database_name: str, function_name: str, api_key: str):
+        pass
