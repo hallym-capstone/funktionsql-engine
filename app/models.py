82fc3ecce7df1aaae1ac3ea9b4595990168a2ce7
Alex Won <dnjscksdn98@gmail.com>
2022-03-28 21:51:35 +0900

    fix: resolve db model type & relationship errors

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
+from datetime import datetime
 from enum import Enum, IntEnum
-from pymysql import Date
 
-from sqlalchemy import JSON, Column, ForeignKey, Integer, String
+from sqlalchemy import JSON, Column, ForeignKey, Integer, String, TIMESTAMP
 from sqlalchemy.orm import relationship
 
 from app.database import Base
@@ -20,16 +20,16 @@ class ExecutionResultType(IntEnum, Enum):
 
 
 class User(Base):
-    __tablename__ = 'Users'
+    __tablename__ = "Users"
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(64), unique=True, nullable=False)
     password = Column(String(64), nullable=False)
-    created_at = Column(Date, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, default=datetime.now())
 
 
 class Auth(Base):
-    __tablename__ = 'Auth'
+    __tablename__ = "Auth"
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
@@ -40,36 +40,36 @@ class Auth(Base):
 
 
 class Database(Base):
-    __tablename__ = 'Databases'
+    __tablename__ = "Databases"
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey(User.id), nullable=False)
     name = Column(String(64), nullable=False)
-    created_at = Column(Date, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, default=datetime.now())
 
     user = relationship("Users")
 
 
 class Function(Base):
-    __tablename__ = 'Functions'
+    __tablename__ = "Functions"
 
     id = Column(Integer, primary_key=True, index=True)
     database_id = Column(Integer, ForeignKey(Database.id), nullable=False)
     name = Column(String(64), nullable=False)
     lambda_key = Column(String(256), nullable=False)
-    created_at = Column(Date, nullable=False)
-    updated_at = Column(Date, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, default=datetime.now())
+    updated_at = Column(TIMESTAMP, nullable=False, default=datetime.now())
 
     database = relationship("Databases")
 
 
 class ExecutionHistory(Base):
-    __tablename__ = 'Functions'
+    __tablename__ = "ExecutionHistory"
 
     id = Column(Integer, primary_key=True, index=True)
     function_id = Column(Integer, ForeignKey(Function.id), nullable=False)
     return_value = Column(String(256), nullable=False)
     type = Column(Integer, nullable=False)
-    created_at = Column(Date, nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, default=datetime.now())
 
     function = relationship("Functions")
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,44 +1,47 @@
-from fastapi import APIRouter, Depends
-from app.models import AuthType
-from datetime import datetime, timedelta
 import jwt
-from starlette.responses import JSONResponse
 import pymysql
+
+from fastapi import APIRouter
+from datetime import datetime, timedelta
+from starlette.responses import JSONResponse
+
+from app.models import AuthType
 
 
 router = APIRouter()
-SECRET_KEY = 'secret_key' #임시
+SECRET_KEY = 'secret_key'  # 임시
 
 
 @router.get("/login", status_code=200)
-async def login(auth_type: AuthType, username: str = None, password: str = None, auth_key = None):
-    if (username is None) or (password is None): #둘 중 하나라도 입력되지 않으면 400 반환
+async def login(auth_type: AuthType, username: str = None, password: str = None, auth_key=None):
+    if (username is None) or (password is None):  # 둘 중 하나라도 입력되지 않으면 400 반환
         return JSONResponse(status_code=400, content=dict(msg="ID or PW Not Provided"))
-    is_exist = await is_info_check(username, password) #해당 계정이 존재하는지 확인
-    if not is_exist: #존재하지 않는다면 400 반환
+    is_exist = await is_info_check(username, password)  # 해당 계정이 존재하는지 확인
+    if not is_exist:  # 존재하지 않는다면 400 반환
         return JSONResponse(status_code=400, content=dict(msg="NO MATCH USER"))
     payload = {
-        'username':username,
-        'exp': datetime.utcnow() + timedelta(hours= 2)
+        'username': username,
+        'exp': datetime.utcnow() + timedelta(hours=2)
     }
-    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8') #토큰생성
-    return JSONResponse(status_code=200, content = token) #토큰 반환
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')  # 토큰생성
+    return JSONResponse(status_code=200, content=token)  # 토큰 반환
 
-async def is_info_check(auth_type: AuthType, username: str,password: str,auth_key):
+
+async def is_info_check(auth_type: AuthType, username: str, password: str, auth_key):
     try:
-        conn = pymysql.connect(host='localhost', user='root', password='1234', db='funcktionsql', charset='utf8') #임시
+        conn = pymysql.connect(host='localhost', user='root', password='1234', db='funcktionsql', charset='utf8')  # 임시
         cur = conn.cursor(pymysql.cursors.DictCursor)
 
         sql = 'SELECT id, username, password FROM Users WHERE username=%s'
         cur.execute(sql, username)
         info = cur.fetchone()
-        if info['username'] is None: #계정 존재 확인
+        if info['username'] is None:  # 계정 존재 확인
             return False
-        if info['password'] != password: #비밀번호 일치여부 확인
+        if info['password'] != password:  # 비밀번호 일치여부 확인
             return False
-        if auth_type != AuthType.NON_SOCIAL: #소셜 로그인인 경우 auth_key 확인
+        if auth_type != AuthType.NON_SOCIAL:  # 소셜 로그인인 경우 auth_key 확인
             sql = 'SELECT auth_key FROM Auth WHERE user_id=%d'
-            cur.execute(sql,info['id'])
+            cur.execute(sql, info['id'])
             if auth_key != cur.fetchone():
                 return False
         return True
