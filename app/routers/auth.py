565ed0123717db9fb9d64cbb1b4af9fdb8ff69c2
chanhee9 <99002860+chanhee9@users.noreply.github.com>
2022-04-03 06:31:49 +0900

    fix login API / add register API and refresh_token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,101 @@
-from fastapi import APIRouter, Depends
+import jwt
+import pymysql
+
+from fastapi import APIRouter
+from datetime import datetime, timedelta
+from starlette.responses import JSONResponse
+
+from app.models import AuthType
 
 
 router = APIRouter()
+SECRET_KEY = 'secret_key'  # 임시
 
 
-@router.get("/login")
-async def login():
-    pass
+@router.get("/login", status_code=200)
+async def login(auth_type: AuthType, username: str = None, password: str = None, token_id: str = None):
+
+    if (username is None) or (password is None):  # 둘 중 하나라도 입력되지 않으면 400 반환
+        return JSONResponse(status_code=400, content=dict(msg="ID or PW Not Provided"))
+
+    try:
+        conn = pymysql.connect(host='localhost', user='root', password='1234!', db='funcktionsql', charset='utf8')  # 임시
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+
+        cur.execute('SELECT id, username, password FROM Users WHERE username = %s', username)
+        info = cur.fetchone()
+
+        if info is None: #계정 존재여부 확인
+            return JSONResponse(status_code=400, content=dict(msg="NO MATCH USER"))
+        else:
+            if info['password'] != password:  # 비밀번호 일치여부 확인
+                return JSONResponse(status_code=400, content=dict(msg="NO MATCH USER"))
+            if auth_type != AuthType.NON_SOCIAL:  # 소셜 로그인인 경우 auth_key 확인
+                cur.execute('SELECT auth_key FROM Auth WHERE user_id = %s', int(info['id']))
+                if token_id != cur.fetchone()['auth_key']:
+                    return JSONResponse(status_code=400, content=dict(msg="NO MATCH USER3"))
+
+    finally:
+        cur.close()
+        conn.close()
+
+    token = create_access_token(username, int(info['id']))
+    create_refresh_token(username, int(info['id']))
+    return JSONResponse(status_code=200, content=token)  # token 반환
+
+
+
+@router.get("/register", status_code=200)
+async def register(username, password, auth_type: AuthType, token_id: str = None):
+    try:
+        conn = pymysql.connect(host='localhost', user='root', password='1234', db='funcktionsql', charset='utf8')  # 임시
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+
+        cur.execute('SELECT * FROM Users WHERE username=%s', username)
+        exist = cur.fetchone()
+        if exist is not None:
+            return JSONResponse(status_code=400, content=dict(msg = "Unavailable ID"))
+
+        cur.execute('INSERT INTO Users (username, password) VALUES (%s,%s)', (username, password))
+        conn.commit()
+        cur.execute('SELECT id FROM Users WHERE username=%s', username)
+        user_id = cur.fetchone()['id']
+        cur.execute('INSERT INTO Auth (user_id, type, auth_key) VALUES (%s, %s, %s)', (user_id, int(auth_type), token_id))
+        conn.commit()
+
+    finally:
+        cur.close()
+        conn.close()
+
+    return JSONResponse(status_code=200, content=dict(msg = "Success"))
+
+
+
+def create_access_token(username, user_id):
+    payload = {
+        'user_id' : user_id,
+        'username' : username,
+        'exp' : datetime.utcnow() + timedelta(hours=2)
+    }
+    access_token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return access_token
+
+
+
+def create_refresh_token(username, user_id):
+    payload = {
+        'user_id' : user_id,
+        'username': username,
+        'exp': datetime.utcnow() + timedelta(hours=24*7)
+    }
+    refresh_token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+    try:
+        conn = pymysql.connect(host='localhost', user='root', password='1234', db='funcktionsql', charset='utf8')  # 임시
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+
+        cur.execute('UPDATE Auth SET refresh_token = %s WHERE user_id = %s', (refresh_token, user_id))
+        conn.commit()
+    finally:
+        cur.close()
+        conn.close()
